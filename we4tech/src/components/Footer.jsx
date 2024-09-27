b1653019243bc95c66962782a754e0121146e2e3
Your Name <Hariharanath247@gmail.com>
2024-09-27 18:43:04 +0530

    hari

--- we4tech/src/components/Footer.jsx
+++ we4tech/src/components/Footer.jsx
@@ -16,7 +16,7 @@ const Footer = () => {
         <div className="footer-section quick-links">
           <h3>Quick Links</h3>
           <ul>
-            <li><a href="/">Home</a></li>
+            <li><a href="/home">Home</a></li>
             <li><a href="/services">Services</a></li>
             <li><a href="/projects">Projects</a></li>
             <li><a href="/about">About Us</a></li>
@@ -41,13 +41,13 @@ const Footer = () => {
             <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
               <FaFacebookF />
             </a>
-            <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+            <a href="https://www.instagram.com/we4tech_?igsh=MWs5aG5jdWZmc2FhZw==" target="_blank" rel="noopener noreferrer">
               <FaInstagram />
             </a>
             <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
               <FaTwitter />
             </a>
-            <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
+            <a href="https://www.linkedin.com/company/we4tech/" target="_blank" rel="noopener noreferrer">
               <FaLinkedinIn />
             </a>
           </div>
