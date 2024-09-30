a5e316e9f2dbd95d5096ea2eeab6119cf4c45a89
Araan Sheikh <90261325+Araan-Sheikh@users.noreply.github.com>
2024-09-30 23:58:38 +0530

    Update App.jsx

--- we4tech/src/App.jsx
+++ we4tech/src/App.jsx
@@ -1,5 +1,6 @@
 
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';import Navbar from './components/Navbar';
+import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import Services from './pages/Services';
