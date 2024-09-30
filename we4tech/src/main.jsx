d989f7d7eef5bc28a7ffe6cc32a59cbe270649ca
Araan Sheikh <90261325+Araan-Sheikh@users.noreply.github.com>
2024-10-01 00:02:21 +0530

    Update main.jsx

--- we4tech/src/main.jsx
+++ we4tech/src/main.jsx
@@ -1,16 +1,10 @@
-// Import necessary modules from React and ReactDOM
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-// Get the root element from the DOM
-const rootElement = document.getElementById('root');
-
-// Create a root and render the App component within StrictMode
-const root = createRoot(rootElement);
-root.render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>
-);
+  </StrictMode>,
+)
