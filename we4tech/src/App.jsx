88a39e1cea46cd24a51f75b06da4a96e285779dc
Araan Sheikh <90261325+Araan-Sheikh@users.noreply.github.com>
2024-09-30 23:52:50 +0530

    Update App.jsx

--- we4tech/src/App.jsx
+++ we4tech/src/App.jsx
@@ -16,7 +16,7 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/services" element={<Services />} />
           <Route path="/projects" element={<Projects />} />
-          <Route path="/aboutus" element={<AboutUs />} />
+          <Route path="/about" element={<AboutUs />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
         <Footer />
